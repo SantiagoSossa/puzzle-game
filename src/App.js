1b5664ea2b58ee488ff6fae411c088b1801a590e
santiagosossa <santiagosossa95@gmail.com>
2019-10-16 11:33:40 -0500

    [feat] first classes
    added first classes to the app

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Puzzle React
         </a>
       </header>
     </div>
