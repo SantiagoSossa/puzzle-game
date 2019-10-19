c88011262d58211dfa6e1c687518a9354687d06f
Bluu <juankmilo_623@hotmail.com>
2019-10-18 19:44:12 -0500

    Logic Of Game Changed, Minor Style Changes

--- src/Game/Game.js
+++ src/Game/Game.js
@@ -5,12 +5,17 @@ import Board from '../Board/Board';
 export default class Game extends Component {
 
     state = {
-        cells: 4,
+        cells: 3,
         movements: 0,
         finished: false,
         collection: [],
         start: false,
-        timer: 0
+        timer: 0,
+        orderedArray: '',
+        zeroColIndex: 0,
+        zeroRowIndex: 0,
+        rowOfZero: 0,
+        colOfZero: 0
     }
 
     chunk = (array, size) => {
@@ -27,67 +32,178 @@ export default class Game extends Component {
     }
 
     handleClick = (e) => {
-        let rowIndex = -1;
-        let columnIndex = -1;
-        const updateCollection = [...this.state.collection];
+
+        const value = e.target.id;
+
+        if (value == 0) {
+            return;
+        }
+
+        let zeroColIndex = this.state.zeroColIndex
+        let zeroRowIndex = this.state.zeroRowIndex
+        let rowOfZero = this.state.rowOfZero
+        let colOfZero = this.state.colOfZero
+
+        let rowIndexClicked = -1;
+        let columnIndexClicked = -1;
+        let rowWhereClicked = -1;
+        let colWhereClicked = -1;
+        let direction = ""
+        let numbers = []
+
+
+        const updateCollection = this.state.collection;
+
+        let moved = false
+
         for (const row in updateCollection) {
-            if (updateCollection[row].indexOf(+e.target.id) >= 0) {
-                rowIndex = updateCollection[row].indexOf(+e.target.id);
-                columnIndex = updateCollection.indexOf(updateCollection[row]);
+            if (updateCollection[row].indexOf(+value) >= 0) {
+                columnIndexClicked = updateCollection[row].indexOf(+value);
+                rowIndexClicked = updateCollection.indexOf(updateCollection[row]);
+                rowWhereClicked = row
+                colWhereClicked = updateCollection[row].indexOf(+value)
+                break;
             }
         }
 
-        //up
-        if (columnIndex > 0) {
-            if (updateCollection[columnIndex - 1][rowIndex] == 0) {
-                updateCollection[columnIndex - 1][rowIndex] = updateCollection[columnIndex][rowIndex];
-                updateCollection[columnIndex][rowIndex] = 0;
-                this.setState({ collection: updateCollection });
+        if (columnIndexClicked == zeroColIndex) {
+
+            let colInicial = columnIndexClicked
+            let rowInicial = rowIndexClicked
+
+            const colZeroInmutable = columnIndexClicked
+            const rowZeroInmutable = rowIndexClicked
+
+            if (rowWhereClicked > rowOfZero) {
+                direction = "down"
+            } else {
+                direction = "up"
             }
-        }
-        //down
-        if (columnIndex < this.state.cells - 1) {
-            if (updateCollection[columnIndex + 1][rowIndex] == 0) {
-                updateCollection[columnIndex + 1][rowIndex] = updateCollection[columnIndex][rowIndex];
-                updateCollection[columnIndex][rowIndex] = 0;
-                this.setState({ collection: updateCollection });
+
+            let cont = 0
+
+            //Current Value
+            let actualValue = updateCollection[rowInicial][colInicial]
+            let nextValue = 0
+
+            while (true) {
+                cont += 1
+                numbers.push(actualValue)
+
+                //Next Value
+                if (direction == "up") {
+                    rowInicial += 1
+                    nextValue = updateCollection[rowInicial][colInicial]
+                } else {
+                    rowInicial -= 1
+                    nextValue = updateCollection[rowInicial][colInicial]
+                }
+
+                if (nextValue == 0) {
+                    for (let i = 0; i < cont; i++) {
+                        if (direction == "up") {
+                            rowIndexClicked += 1
+                            updateCollection[rowIndexClicked][columnIndexClicked] = numbers[i]
+                        } else {
+                            rowIndexClicked -= 1
+                            updateCollection[rowIndexClicked][columnIndexClicked] = numbers[i]
+                        }
+                    }
+                    updateCollection[rowZeroInmutable][colZeroInmutable] = 0
+                    this.setState({ collection: updateCollection, zeroColIndex: colZeroInmutable, zeroRowIndex: rowZeroInmutable, rowOfZero: rowZeroInmutable });
+                    moved = true
+                    break;
+                }
+                actualValue = nextValue
             }
-        }
-        //left
-        if (rowIndex > 0) {
-            if (updateCollection[columnIndex][rowIndex - 1] == 0) {
-                updateCollection[columnIndex][rowIndex - 1] = updateCollection[columnIndex][rowIndex];
-                updateCollection[columnIndex][rowIndex] = 0;
-                this.setState({ collection: updateCollection });
+        } else if (rowIndexClicked == zeroRowIndex) {
+            console.log("si")
+
+            let colInicial = columnIndexClicked
+            let rowInicial = rowIndexClicked
+
+            const colZeroInmutable = columnIndexClicked
+            const rowZeroInmutable = rowIndexClicked
+
+            if (colWhereClicked > colOfZero) {
+                direction = "right"
+            } else {
+                direction = "left"
             }
-        }
-        //right
-        if (rowIndex < this.state.cells - 1) {
-            if (updateCollection[columnIndex][rowIndex + 1] == 0) {
-                updateCollection[columnIndex][rowIndex + 1] = updateCollection[columnIndex][rowIndex];
-                updateCollection[columnIndex][rowIndex] = 0;
-                this.setState({ collection: updateCollection });
+
+            let cont = 0
+
+            //Current Value
+            let actualValue = updateCollection[rowInicial][colInicial]
+            let nextValue = 0
+
+            while (true) {
+                cont += 1
+                numbers.push(actualValue)
+
+                //Next Value
+                if (direction == "right") {
+                    colInicial -= 1
+                    nextValue = updateCollection[rowInicial][colInicial]
+                } else {
+                    colInicial += 1
+                    nextValue = updateCollection[rowInicial][colInicial]
+                }
+
+                if (nextValue == 0) {
+                    for (let i = 0; i < cont; i++) {
+                        if (direction == "right") {
+                            columnIndexClicked -= 1
+                            updateCollection[rowIndexClicked][columnIndexClicked] = numbers[i]
+                        } else {
+                            columnIndexClicked += 1
+                            updateCollection[rowIndexClicked][columnIndexClicked] = numbers[i]
+                        }
+                    }
+                    updateCollection[rowZeroInmutable][colZeroInmutable] = 0
+                    this.setState({ collection: updateCollection, zeroColIndex: colZeroInmutable, zeroRowIndex: rowZeroInmutable, colOfZero: colZeroInmutable });
+                    moved = true
+                    break;
+                }
+                actualValue = nextValue
             }
         }
 
-        const value = e.target.id
-        if (value != 0) {
+        //Update Movements
+        if (value != 0 && moved) {
             this.setState({
                 movements: this.state.movements + 1
             })
         }
 
-        const originalArray = JSON.stringify([...Array(Math.pow((this.state.cells), 2)).keys()]);
-        const resolved = originalArray.substr(1, originalArray.length - 2) == updateCollection.join(",")
+        moved = false
+
+        //Check if game is resolved
+        const resolved = this.state.orderedArray == updateCollection.join(",")
         if (resolved) {
             clearInterval(this.intervalTimer)
             this.setState({
                 finished: resolved
             });
+
+            //get all divs and disable'em
+            const divs = document.querySelectorAll(".cell");
+            divs.forEach(div => {
+                div.classList.add('disabled');
+            });
+
         }
+
     }
 
-    startGame = () => {
+    startGame = async () => {
+
+        //Check for a correct cells value
+        if (!this.state.cells || this.state.cells < 3) {
+            await this.setState({
+                cells: 3
+            })
+        }
 
         //Create, shuffle and state the new random numbers array and start the game
         var shuffle = require('shuffle-array');
@@ -95,25 +211,50 @@ export default class Game extends Component {
         shuffle(array);
         const collection = this.chunk(array, this.state.cells);
 
+        let orderedArray = JSON.stringify([...Array(Math.pow((this.state.cells), 2)).keys()]);
+        orderedArray = orderedArray.substr(1, orderedArray.length - 2)
+        orderedArray += ",0"
+        orderedArray = orderedArray.substr(2)
+        this.setState({
+            orderedArray
+        })
+
+        let columnIndex = 0;
+        let rowIndex = 0;
+        let rowOfZero = 0;
+        let colOfZero = 0;
+
+        for (const row in collection) {
+            if (collection[row].indexOf(0) != -1) {
+                columnIndex = collection[row].indexOf(0);
+                rowIndex = collection.indexOf(collection[row]);
+                rowOfZero = row
+                colOfZero = collection[row].indexOf(0)
+                break;
+            }
+        }
+
         //Animations
         const game = document.getElementById("game");
         const title = document.querySelector(".mainTitle").children[0];
         // eslint-disable-next-line no-restricted-globals
         const width = screen.width;
         game.style.paddingTop = '0%';
-        if (width > 992){
+        if (width > 992) {
             title.style.fontSize = '4rem';
-        } else if (width > 320){
+        } else if (width > 320) {
             title.style.fontSize = '2rem';
         }
-        
+
 
         //Start Timers
         setTimeout(() => {
             this.timerStart()
-            this.setState({ collection, start: true });
-            document.getElementById("board").scrollIntoView({block: 'start',  behavior: 'smooth' });
+            this.setState({ collection, start: true, zeroColIndex: columnIndex, zeroRowIndex: rowIndex, rowOfZero, colOfZero });
+            document.getElementById("board").scrollIntoView({ block: 'start', behavior: 'smooth' });
         }, 500);
+
+
     }
 
     intervalTimer = 0;
@@ -130,9 +271,37 @@ export default class Game extends Component {
         clearInterval(this.timerStart)
     }
 
+    onChange = ({ target }) => {
+        let val = target.value
+        if (val === "") {
+            val = String(val)
+        } else {
+            val = parseInt(val)
+        }
+        this.setState({
+            cells: val
+        })
+    }
+
+    handlerRestart = () => {
+        clearInterval(this.intervalTimer);
+        this.setState({
+            timer: 0,
+            movements: 0,
+            finished: false
+        })
+        this.startGame();
+
+        //get all divs and disable'em
+        const divs = document.querySelectorAll(".cell");
+        divs.forEach(div => {
+            div.classList.remove('disabled');
+        });
+    }
+
     render() {
 
-        const isFinished = this.state.finished ? <h3>You Win!</h3> : null;
+        const isFinished = this.state.finished ? <div className="success"><h3>You Win!</h3></div> : null;
 
         return (
             <div className="unselectable" id="game">
@@ -145,7 +314,7 @@ export default class Game extends Component {
                             <div className="statContainer">
                                 <div className="header">
                                     Stats
-                            </div>
+                                </div>
                                 <div className="statTitle">
                                     Movements &emsp; <span className="stat">{this.state.movements}</span>
                                 </div>
@@ -153,12 +322,21 @@ export default class Game extends Component {
                                     Timer &emsp; <span className="stat">{this.state.timer}</span>
                                 </div>
                             </div>
+                            <div className="restartContainer">
+                                <button onClick={this.handlerRestart} className="restartBtn">Restart</button>
+                            </div>
+                            {isFinished}
                         </div> :
                         null
                     }
                     {this.state.start ? <Board row={this.state.cells} collection={this.state.collection} clicked={this.handleClick} /> : null}
-                    {isFinished}
-                    {this.state.start ? null : <button className="startBtn" onClick={this.startGame}>Start</button>}
+                    {this.state.start ? null :
+                        <div className="cellControllers">
+                            <div className="labelContainer"><label htmlFor="numCellsInput">Numero de celdas</label></div>
+                            <input value={this.state.cells} onChange={this.onChange} id="numCellsInput" type="number" />
+                            <button className="startBtn" onClick={this.startGame}>Start</button>
+                        </div>
+                    }
                 </div>
             </div>
         )
